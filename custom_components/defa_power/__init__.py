3b5b615df363e7b0e866007af30fa632c53b293d
Bebbssos <benjamin.lund@outlook.com>
2024-10-20 17:10:06 +0000

    removed async_setup

--- custom_components/defa_power/__init__.py
+++ custom_components/defa_power/__init__.py
@@ -1,18 +1,10 @@
+"""DEFA Power integration for Home Assistant."""
+
 import logging
 
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
-
-from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
-
-
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the DEFA Power integration."""
-    _LOGGER.info("Setting up DEFA Power integration")
-    hass.data[DOMAIN] = {}
-    return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry) -> bool:
